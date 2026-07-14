8c71a0b4bbebecb6a2057be1da305765c067bc24
Ilya Tikhonovskiy <loislo@google.com>
2026-07-14 01:04:48 -0700

    Rename xla_gpu_default_to_alg_dot_bf16_bf16_f32 to xla_gpu_match_tpu_precision.
    This renames the debug option flag to more accurately describe its effect: using a lower precision for dot operations on GPU, similar to the default precision used on TPU.
    
    Reverts 3136599ab8b81d1e481e4b2e4a2debdcbee13531
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/123020 from madib06ops:slice-shape-mismatch ef27a6b0ce9c170017d12f84a82057c9eed64430
    PiperOrigin-RevId: 947507955

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -392,7 +392,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_experimental_enable_fusion_block_level_rewriter(false);
 
-  opts.set_xla_gpu_default_to_alg_dot_bf16_bf16_f32(false);
+  opts.set_xla_gpu_match_tpu_precision(false);
   opts.set_xla_gpu_enable_libnvptxcompiler(
       stream_executor::IsLibNvPtxCompilerSupported());
   opts.set_xla_gpu_libnvjitlink_mode(DebugOptions::LIB_NV_JIT_LINK_MODE_AUTO);
@@ -1879,12 +1879,11 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       "threads. Setting to 0 (the default value) means no enforcement."));
 
   flag_list->push_back(tsl::Flag(
-      "xla_gpu_default_to_alg_dot_bf16_bf16_f32",
-      bool_setter_for(
-          &DebugOptions::set_xla_gpu_default_to_alg_dot_bf16_bf16_f32),
-      debug_options->xla_gpu_default_to_alg_dot_bf16_bf16_f32(),
+      "xla_gpu_match_tpu_precision",
+      bool_setter_for(&DebugOptions::set_xla_gpu_match_tpu_precision),
+      debug_options->xla_gpu_match_tpu_precision(),
       "Use the dot precision algorithm `ALG_DOT_BF16_BF16_F32 by default for "
-      "f32 dots."));
+      "f32 dots. This leads to the same precision as on TPU."));
   flag_list->push_back(
       tsl::Flag("xla_gpu_deterministic_ops",
                 bool_setter_for(&DebugOptions::set_xla_gpu_deterministic_ops),
